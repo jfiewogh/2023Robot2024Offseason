722df16afbde01c6499030d73d34a11c0cbb66eb
David Chau <86276480+jfiewogh@users.noreply.github.com>
2024-10-17 18:59:21 -0500

    Run motors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,9 +7,12 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -20,11 +23,9 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final ArmSubsystem armSubsystem = new ArmSubsystem();
+  
+  private final Joystick joystick = new Joystick(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -32,23 +33,15 @@ public class RobotContainer {
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    new JoystickButton(joystick, 1).onTrue(new InstantCommand(() -> {
+      armSubsystem.liftArm(1);
+      armSubsystem.runElbow(1);
+    }));
+    new JoystickButton(joystick, 2).onTrue(new InstantCommand(() -> {
+      armSubsystem.liftArm(-1);
+      armSubsystem.runElbow(-1);
+    }));
   }
 
   /**
@@ -56,8 +49,8 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+  // public Command getAutonomousCommand() {
+  //   // An example command will be run in autonomous
+  //   // return Autos.exampleAuto(m_exampleSubsystem);
+  // }
 }
---
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class ArmSubsystem {
+    private CANSparkMax liftArmMotor = new CANSparkMax(8, MotorType.kBrushless);
+    private CANSparkMax rotateArmMotor1 = new CANSparkMax(16, MotorType.kBrushless);
+    private CANSparkMax rotateArmMotor2 = new CANSparkMax(18, MotorType.kBrushless);
+    
+    public void runElbow(double speed) {
+        rotateArmMotor1.set(-speed);
+        rotateArmMotor2.set(speed);
+    }
+
+    public void liftArm(double speed) {
+        liftArmMotor.set(-speed);
+    }
+}
