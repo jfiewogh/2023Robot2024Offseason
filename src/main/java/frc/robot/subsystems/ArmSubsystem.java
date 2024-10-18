300d3b090a0955df7141258cb8419ac451c6ea88
David Chau <86276480+jfiewogh@users.noreply.github.com>
2024-10-18 16:12:51 -0500

    Changed name to elbow motor

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,12 +5,12 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class ArmSubsystem {
     private CANSparkMax liftArmMotor = new CANSparkMax(8, MotorType.kBrushless);
-    private CANSparkMax rotateArmMotor1 = new CANSparkMax(16, MotorType.kBrushless);
-    private CANSparkMax rotateArmMotor2 = new CANSparkMax(18, MotorType.kBrushless);
+    private CANSparkMax elbowMotor1 = new CANSparkMax(16, MotorType.kBrushless);
+    private CANSparkMax elbowMotor2 = new CANSparkMax(18, MotorType.kBrushless);
     
     public void runElbow(double speed) {
-        rotateArmMotor1.set(-speed);
-        rotateArmMotor2.set(speed);
+        elbowMotor1.set(-speed);
+        elbowMotor2.set(speed);
     }
 
     public void liftArm(double speed) {
